f9fb0991a7dd4552953e61ba9932983a4ba6d954
kelvin13 <kelvin13ma@gmail.com>
2022-06-11 18:37:46 -0500

    fourth commit

--- sources/foo/foo.swift
+++ sources/foo/foo.swift
@@ -1,13 +1,7 @@
 /// A symbol that exists in all versions of this package.
 /// 
-/// The documentation for this symbol was last updated in version `0.1.1`.
+/// The documentation for this symbol was last updated in version `0.3.0`.
 public 
 enum Permanent
-{
-}
-
-/// This symbol should only appear in version `0.2.0`.
-public 
-enum Foo 
 {
 }
